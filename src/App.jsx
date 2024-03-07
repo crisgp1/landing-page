db221c7b220549d662f6e291796a5e948b258790
Cristian <cristiangp2001@gmail.com>
2024-03-07 02:19:21 -0600

    Update buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,20 @@
 export default function App() {
   return (
-      <div className="flex justify-center items-center h-screen">
-        <div className="text-center">
-          <h1 className="text-4xl font-bold text-white">CliquéaloMX</h1>
-          <p className="text-2xl text-white mt-3">La nueva forma de comprar tu auto está en construcción</p>
-          <p className="text-2xl font-bold text-white mt-8">Consulta nuestro inventario</p>
+      <div className="flex justify-center items-center min-h-screen bg-gray-900 text-white">
+        <div className="text-center m-5">
+          <h1 className="text-4xl font-bold">CliquéaloMX</h1>
+          <p className="text-lg mt-5">La nueva forma de comprar tu auto en construcción</p>
+          <p className="text-lg mt-2">Consulta nuestro inventario</p>
 
           {/* Buttons */}
-          <div className="flex justify-center space-x-4 mt-8">
-            <a href="https://www.mercadolibre.com.mx/perfil/CLIQUEALO+MX" target="_blank" rel="noopener noreferrer" className="border border-2 border-amber-500 text-amber-500 hover:bg-amber-500 hover:text-white font-bold py-2 px-4 rounded transition duration-300 ease-in-out">
+          <div className="flex flex-col space-y-4 mt-8 m-5">
+            <a href="https://www.mercadolibre.com.mx/perfil/CLIQUEALO+MX" target="_blank" rel="noopener noreferrer" className="border border-2 border-amber-500 text-amber-500 hover:bg-amber-500 hover:text-white font-bold py-2 px-4 rounded transition duration-300 ease-in-out w-full md:w-auto">
               MercadoLibre
             </a>
-            <a href="https://www.instagram.com/cliquealo/" target="_blank" rel="noopener noreferrer" className="border border-2 border-pink-500 text-pink-500 hover:bg-pink-500 hover:text-white font-bold py-2 px-4 rounded transition duration-300 ease-in-out">
+            <a href="https://www.instagram.com/cliquealo/" target="_blank" rel="noopener noreferrer" className="border border-2 border-pink-500 text-pink-500 hover:bg-pink-500 hover:text-white font-bold py-2 px-4 rounded transition duration-300 ease-in-out w-full md:w-auto">
               Instagram
             </a>
-            <a href="https://wa.me/+523315670227" target="_blank" rel="noopener noreferrer" className="border border-2 border-green-500 text-green-500 hover:bg-green-500 hover:text-white font-bold py-2 px-4 rounded transition duration-300 ease-in-out">
+            <a href="https://wa.me/+523315670227" target="_blank" rel="noopener noreferrer" className="border border-2 border-green-500 text-green-500 hover:bg-green-500 hover:text-white font-bold py-2 px-4 rounded transition duration-300 ease-in-out w-full md:w-auto">
               WhatsApp
             </a>
           </div>
